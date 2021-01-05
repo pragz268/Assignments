8598eeb85fa70adf2f67f9409a163d4f886e65de
pragz268 <54812153+pragz268@users.noreply.github.com>
2021-01-05 14:01:32 +0100

    Add files via upload

---
+++ asset.sql
@@ -0,0 +1,33 @@
+CREATE TABLE asset
+(
+   id int PRIMARY KEY NOT NULL,
+   asset_name varchar(255),
+   end_time timestamp,
+   severity int,
+   start_time timestamp
+)
+;
+CREATE UNIQUE INDEX PRIMARY ON asset(id)
+;
+CREATE TABLE asset
+(
+   id int PRIMARY KEY NOT NULL,
+   asset_name varchar(255),
+   end_time timestamp,
+   severity int,
+   start_time timestamp
+)
+;
+CREATE UNIQUE INDEX PRIMARY ON asset(id)
+;
+CREATE TABLE asset
+(
+   id int PRIMARY KEY NOT NULL,
+   asset_name varchar(255),
+   end_time timestamp,
+   severity int,
+   start_time timestamp
+)
+;
+CREATE UNIQUE INDEX PRIMARY ON asset(id)
+;
